91a18be2943e53f2fa3867e29ad68e137823ee54
thiagobarrera <54426422+thiagobarrera@users.noreply.github.com>
2021-02-28 18:58:35 -0300

    Models

---
+++ eggplant-brownie/Models/Item.swift
@@ -0,0 +1,36 @@
+//
+//  Item.swift
+//  eggplant-brownie
+//
+//  Created by Alura on 16/03/19.
+//  Copyright © 2019 Alura. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject, NSCoding {
+    
+    // MARK: - Atributos
+    
+    let nome: String
+    let calorias: Double
+    
+    // MARK: - Init
+    
+    init(nome: String, calorias: Double) {
+        self.nome = nome
+        self.calorias = calorias
+    }
+    
+    // MARK: - NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(nome, forKey: "nome")
+        aCoder.encode(calorias, forKey: "calorias")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        nome = aDecoder.decodeObject(forKey: "nome") as! String
+        calorias = aDecoder.decodeDouble(forKey: "calorias")
+    }
+}
---
+++ eggplant-brownie/Models/Refeicao.swift
@@ -0,0 +1,62 @@
+//
+//  Refeicao.swift
+//  eggplant-brownie
+//
+//  Created by Alura on 16/03/19.
+//  Copyright © 2019 Alura. All rights reserved.
+//
+
+import UIKit
+
+class Refeicao: NSObject, NSCoding {
+    
+    // MARK: - Atributos
+    
+    let nome: String
+    let felicidade: Int
+    var itens: Array<Item> = []
+    
+    // MARK: - Init
+    
+    init(nome: String, felicidade: Int, itens: [Item] = []) {
+        self.nome = nome
+        self.felicidade = felicidade
+        self.itens = itens
+    }
+    
+    // MARK: - NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(nome, forKey: "nome")
+        aCoder.encode(felicidade, forKey: "felicidade")
+        aCoder.encode(itens, forKey: "itens")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        nome = aDecoder.decodeObject(forKey: "nome") as! String
+        felicidade = aDecoder.decodeInteger(forKey: "felicidade")
+        itens = aDecoder.decodeObject(forKey: "itens") as! Array<Item>
+    }
+    
+    // MARK: - Metodos
+    
+    func totalDeCalorias() -> Double {
+        var total = 0.0
+        
+        for item in itens {
+            total += item.calorias
+        }
+        
+        return total
+    }
+    
+    func detalhes() -> String {
+        var mensagem = "Felicidade: \(felicidade)"
+        
+        for item in itens {
+            mensagem += ", \(item.nome) - calorias: \(item.calorias)"
+        }
+        
+        return mensagem
+    }
+}
